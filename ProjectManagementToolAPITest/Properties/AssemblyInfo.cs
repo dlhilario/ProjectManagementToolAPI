10fd132215b2cc56058caa41e0d15c55b34789d5
Domingo Hilario <domingo.hilario@outlook.com>
2019-05-03 16:33:00 -0400

    Pushing Test

---
+++ ProjectManagementToolAPITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectManagementToolAPITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ProjectManagementToolAPITest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eff7074a-839d-4aae-8b18-cd08c388b5c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjectManagementToolAPITest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using projectManagementToolWebAPI;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace projectManagementToolWebAPI.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        PGMTWebService _PMTWebAPI;
+        public UnitTest1()
+        {
+            _PMTWebAPI = new PGMTWebService();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            var result =_PMTWebAPI.Login("dlhilario", "ando1977");
+            Assert.IsNotNull(result, "Unable to login");
+        }
+    }
+}
+
